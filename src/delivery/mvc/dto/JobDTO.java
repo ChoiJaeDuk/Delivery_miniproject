e6a2f158d19237431e790e49e3baa63a83fb4336
jongmin <34manta@naver.com>
2022-09-03 21:58:09 +0900

    ver02-1 이름 : 이종민 내용 : DTO추가 categories, delivery_status, job

--- src/delivery/mvc/dto/JobDTO.java
+++ src/delivery/mvc/dto/JobDTO.java
@@ -1,5 +1,44 @@
 package delivery.mvc.dto;
 
 public class JobDTO {
+	private int job_code;
+	private int job_name;
+	
+	public JobDTO () { }
 
+	public JobDTO(int job_code, int job_name) {
+		super();
+		this.job_code = job_code;
+		this.job_name = job_name;
+	}
+
+	public int getJob_code() {
+		return job_code;
+	}
+
+	public void setJob_code(int job_code) {
+		this.job_code = job_code;
+	}
+
+	public int getJob_name() {
+		return job_name;
+	}
+
+	public void setJob_name(int job_name) {
+		this.job_name = job_name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("JobDTO [job_code=");
+		builder.append(job_code);
+		builder.append(", job_name=");
+		builder.append(job_name);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
 }
--- src/delivery/mvc/dto/OrdersDTO.java
+++ src/delivery/mvc/dto/OrdersDTO.java
@@ -21,6 +21,70 @@ public class OrdersDTO {
 		this.order_delivery_time = order_delivery_time;
 		this.delivery_code = delivery_code;
 	}
+	public int getOrder_code() {
+		return order_code;
+	}
+	public void setOrder_code(int order_code) {
+		this.order_code = order_code;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(String order_date) {
+		this.order_date = order_date;
+	}
+	public int getOrder_total_price() {
+		return order_total_price;
+	}
+	public void setOrder_total_price(int order_total_price) {
+		this.order_total_price = order_total_price;
+	}
+	public String getOrder_approval_time() {
+		return order_approval_time;
+	}
+	public void setOrder_approval_time(String order_approval_time) {
+		this.order_approval_time = order_approval_time;
+	}
+	public String getOrder_delivery_time() {
+		return order_delivery_time;
+	}
+	public void setOrder_delivery_time(String order_delivery_time) {
+		this.order_delivery_time = order_delivery_time;
+	}
+	public int getDelivery_code() {
+		return delivery_code;
+	}
+	public void setDelivery_code(int delivery_code) {
+		this.delivery_code = delivery_code;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("OrdersDTO [order_code=");
+		builder.append(order_code);
+		builder.append(", user_id=");
+		builder.append(user_id);
+		builder.append(", order_date=");
+		builder.append(order_date);
+		builder.append(", order_total_price=");
+		builder.append(order_total_price);
+		builder.append(", order_approval_time=");
+		builder.append(order_approval_time);
+		builder.append(", order_delivery_time=");
+		builder.append(order_delivery_time);
+		builder.append(", delivery_code=");
+		builder.append(delivery_code);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 	
 	
 }
